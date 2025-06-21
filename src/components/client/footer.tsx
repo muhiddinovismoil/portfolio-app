1996460a68ac66f93c5a523ac44265de120de8df
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-06-21 16:35:15 +0500

    footer responsive successfully finished

--- src/components/client/footer.tsx
+++ src/components/client/footer.tsx
@@ -6,17 +6,19 @@ import { useTheme } from "next-themes";
 export const ClientFooter = () => {
     const { theme } = useTheme();
     return (
-        <footer className="pb-[26px]">
+        <footer className="pb-[26px] md:pb-[32px] ">
             <div className="container">
-                <div className="flex flex-col px-[44px]">
+                <div className="flex flex-col px-[44px] max-w-[876px] mx-auto">
                     <div className="flex justify-between">
-                        <h4 className="font-bold text-[14px]">Follow me</h4>
-                        <div className="flex items-center gap-[11px]">
+                        <h4 className="font-bold text-[14px] md:text-[16px] xl:text-[18px]">
+                            Follow me
+                        </h4>
+                        <div className="flex items-center gap-[11px] xl:gap-[14px]">
                             <Link href={"mailto:mukhiddinovvismoil@gmail.com"}>
                                 <Image
                                     width={20}
                                     height={16}
-                                    className=""
+                                    className="xl:w-[24px] xl:h-[20px]"
                                     src={
                                         theme === "light"
                                             ? "/footer/mail.dark.svg"
@@ -32,7 +34,7 @@ export const ClientFooter = () => {
                                 <Image
                                     width={20}
                                     height={19.52}
-                                    className=""
+                                    className="xl:w-[24px] xl:[h-20px]"
                                     src={
                                         theme === "light"
                                             ? "/footer/github.dark.svg"
@@ -50,7 +52,7 @@ export const ClientFooter = () => {
                                 <Image
                                     width={18}
                                     height={18}
-                                    className=""
+                                    className="xl:w-[24px] xl:h-[22px]"
                                     src={
                                         theme === "light"
                                             ? "/footer/linkedin.dark.svg"
@@ -64,12 +66,14 @@ export const ClientFooter = () => {
                     <div
                         className={
                             theme === "dark"
-                                ? "bg-white rounded-[10px] mt-[6.48px] mb-[6px] py-[1.5px]"
-                                : "bg-black rounded-[10px] mt-[6.48px] mb-[6px] py-[1.5px]"
+                                ? "bg-white rounded-[10px] mt-[6.48px] py-[1.5px] md:py-[2px] md:mt-[9px]"
+                                : "bg-black rounded-[10px] mt-[6.48px] py-[1.5px] md:py-[2px] md:mt-[9px]"
                         }
                     />
-                    <div className="flex justify-center items-center">
-                        <p className="text-[8px]">Muhiddinov Ismoil © 2025</p>
+                    <div className="flex justify-center items-center mt-[6px] md:mt-[8px]">
+                        <p className="text-[8px] md:text-[9px] xl:text-[11px]">
+                            Muhiddinov Ismoil © 2025
+                        </p>
                     </div>
                 </div>
             </div>
