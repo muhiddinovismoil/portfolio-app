9158e7a4543bf73d351e312a6f956c1069f7e05a
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-06-22 18:39:24 +0500

    portfolio site main page was successfully finshed with responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,24 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main>
-            <section className="pt-[151px] pb-[257px]">
+            <section className="pt-[151px] pb-[257px] lg:pt-[200px] lg:pb-[140px] min-[1440px]:pt-[220px] min-[1440px]:pb-[150px]">
                 <div className="container">
-                    <div className="flex flex-col px-[35px] items-center gap-[44px]">
-                        <div>
+                    <div className="flex flex-col px-[35px] items-center gap-[44px] lg:flex-row lg:justify-around xl:gap-0">
+                        <div className="lg:w-[400px] xl:w-[412px] xl:h-[394px]">
                             <Image
                                 src={"/hero/my-image.svg"}
                                 width={175}
                                 height={169}
                                 alt="my image"
+                                className="lg:w-[400px] lg:h-[350px] xl:w-full xl:h-full"
                             />
                         </div>
-                        <div className="flex flex-col gap-[54px]">
-                            <div className="flex flex-col gap-[23px] text-center">
-                                <h1 className="text-[31px] font-bold tracking-[0.04em]">
+                        <div className="flex flex-col gap-[54px] lg:w-[450px] xl:w-[535px] min-[1440px]:gap-[102px]">
+                            <div className="flex flex-col gap-[23px] text-center lg:text-start min-[1440px]:w-[617px]">
+                                <h1 className="text-[31px] font-bold tracking-[0.04em] xl:text-[42px]">
                                     Hi, I'm Ismoil
                                 </h1>
-                                <p className="text-[19px] leading-[142%] tracking-[0.03em]">
+                                <p className="text-[19px] leading-[142%] tracking-[0.03em] xl:text-[21px] xl:leading-[183%] xl:tracking-[0.03em]">
                                     During{" "}
                                     <span className="font-bold">
                                         the past year
@@ -35,7 +36,7 @@ export default function Home() {
                                     build web applications.
                                 </p>
                             </div>
-                            <div className="flex flex-col gap-[23px] items-center">
+                            <div className="flex flex-col gap-[23px] items-center lg:flex-row">
                                 <a
                                     href="/cv/MuhiddinovIsmoil-Resume.pdf"
                                     download
@@ -46,7 +47,9 @@ export default function Home() {
                                         src="/button-icons/download.icon.svg"
                                         alt="download icon"
                                     />
-                                    Download CV
+                                    <span className="lg:text-[12px] xl:text-[16px]">
+                                        Download CV
+                                    </span>
                                 </a>
                                 <Link
                                     href={"/projects"}
@@ -57,7 +60,9 @@ export default function Home() {
                                         src="/button-icons/projects.btn.icon.svg"
                                         alt="download icon"
                                     />
-                                    See Projects
+                                    <span className="lg:text-[12px] xl:text-[16px]">
+                                        See Projects
+                                    </span>
                                 </Link>
                             </div>
                         </div>
