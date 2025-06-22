c386c4bb494ca079563a533741d0f25783f8bc1e
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-06-22 23:08:41 +0500

    static data successfully finished and raedy to build

--- src/data/stack-techno.data.ts
+++ src/data/stack-techno.data.ts
@@ -4,16 +4,16 @@ export const StackTechno: StackTechnoI[] = [
     {
         id: "b1e8a1c2-1f23-4d5a-8e9b-1c2d3e4f5a6b",
         title: "Node.js",
-        techIcon: "",
+        techIcon: "/about/nodejs.svg",
     },
     {
         id: "c3d4e5f6-7a8b-9c0d-1e2f-3a4b5c6d7e8f",
         title: "React.js",
-        techIcon: "",
+        techIcon: "/about/reactjs.svg",
     },
     {
         id: "d5e6f7a8-9b0c-1d2e-3f4a-5b6c7d8e9f0a",
         title: "Coffee",
-        techIcon: "",
+        techIcon: "/about/coffee.svg",
     },
 ];
