d1d0c5f910d7684c69db912997cbdd815e564c33
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-06-24 11:16:03 +0500

    education page successfully finished with responsive

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,12 +1,28 @@
+import { EductaionUi } from "@/components/ui/education";
+import { Education } from "@/data";
+import Link from "next/link";
 import React from "react";
 
 const Eduaction = () => {
     return (
         <>
-            <section className="pt-[150px]">
+            <section className="pt-[150px] pb-[100px]">
                 <div className="container">
-                    <div className="pl-[30px]">
+                    <div className="px-[30px] flex flex-col gap-[55px]">
                         <h2 className="text-[29px] font-bold">Education</h2>
+                        <div className="flex flex-col gap-[45px]">
+                            {Education.map((item) => (
+                                <EductaionUi
+                                    id={item.id}
+                                    key={item.id}
+                                    link={item.link}
+                                    title={item.title}
+                                    endDate={item.endDate}
+                                    subTitle={item.subTitle}
+                                    startDate={item.startDate}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </section>
--- src/components/ui/education.tsx
+++ src/components/ui/education.tsx
@@ -1,5 +1,6 @@
-import { EducationI } from "@/data";
 import React from "react";
+import Link from "next/link";
+import { EducationI } from "@/data";
 
 export const EductaionUi = ({
     id,
@@ -9,5 +10,35 @@ export const EductaionUi = ({
     subTitle,
     startDate,
 }: EducationI) => {
-    return <div>EductaionUi</div>;
+    return (
+        <div className="flex items-center justify-between border rounded-[21px] py-[18px] pl-[10px] pr-[10px] min-[375px]:px-[15px] sm:px-[20px] sm:py-[22px] lg:px-[30px]">
+            <div className="flex flex-col gap-[62px] sm:gap-[42px] lg:gap-[38px]">
+                <div className="flex flex-col gap-[10px]">
+                    <h3 className="text-[20px] font-bold sm:text-[22px] lg:text-[24px]">
+                        {title}
+                    </h3>
+                    <h4 className="text-[14px] font-medium text-[#767676] dark:text-[#b2b2b2] sm:text-[16px] lg:text-[18px]">
+                        {subTitle}
+                    </h4>
+                </div>
+                <div className="flex flex-col gap-[5px]">
+                    <p className="text-[14px] font-medium sm:text-[16px] lg:text-[18px]">
+                        Date:
+                    </p>
+                    <p className="text-[12px] font-medium text-[#767676] dark:text-[#b2b2b2] sm:text-[14px] lg:text-[16px]">
+                        {startDate} — {endDate}
+                    </p>
+                </div>
+            </div>
+            <div className="">
+                <Link
+                    href={link}
+                    target="_blank"
+                    className="text-[10px] min-[375px]:text-[16px] sm:text-[16px] lg:text-[18px]"
+                >
+                    View Certificate
+                </Link>
+            </div>
+        </div>
+    );
 };
